84e48e9b1fa49fcadd91eb0fcb83b75a2e35d4c0
empathetic-parameter-3158 <nilagathp@gmail.com>
2019-04-13 16:36:34 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,5 @@
 # Build your say_hello method here
-def say_hello (name,programmer)
-  puts "Hello, #"
+def say_hello (name, programmer)
+  puts "Hello, #{name}!"
+end
+programmer = "Ruby Programmer"
